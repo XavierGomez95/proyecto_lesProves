958ac310ffa588fe319c8fe8e7868ecdf12692dc
XavierGomez <xavierg.gomez@students.salle.url.edu>
2021-12-29 23:01:27 +0100

    LesProvesDAO

--- src/persistence/LesProvesDAO.java
+++ src/persistence/LesProvesDAO.java
@@ -1,16 +1,21 @@
 package persistence;
 
-import business.Player;
+import business.*;
 import com.google.gson.Gson;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LesProvesDAO {
+    private final static int ARTICLE = 1;
+    private final static int MASTER = 2;
+    private final static int PHD = 3;
+    private final static int BUDGET = 4;
     private static Gson gson = new Gson();
-
-    private List<Player[]> playersList = new ArrayList<>();
+    private List<Trial[]> trialsList = new ArrayList<>();
 
     private File publicArticleCsvFile = new File("csv_files/publicArticle.csv");
     private File studyMasterCsvFile = new File("csv_files/studyMaster.csv");
@@ -26,17 +31,30 @@ public class LesProvesDAO {
 
     }
 
-    public List<Player[]> readJson () {
+    public List<Trial[]> readJson () {
+        try {
+            FileReader fileReader1 = new FileReader(publicArticleJsonFile);
+            FileReader fileReader2 = new FileReader(studyMasterJsonFile);
+            FileReader fileReader3 = new FileReader(phdDefenseJsonFile);
+            FileReader fileReader4 = new FileReader(budgetRequestJsonFile);
 
-        return playersList;
+            trialsList.add(gson.fromJson(fileReader1, PublicArticle[].class));
+            trialsList.add(gson.fromJson(fileReader2, StudyMaster[].class));
+            trialsList.add(gson.fromJson(fileReader3, PhDefense[].class));
+            trialsList.add(gson.fromJson(fileReader4, BudgedRequest[].class));
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+
+        return trialsList;
     }
 
     public void writeCsv () {
 
     }
 
-    public List<Player[]> readCsv () {
+    public List<Trial[]> readCsv () {
 
-        return playersList;
+        return trialsList;
     }
 }
