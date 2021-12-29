7e0f01abddcb19ff614d9422025b98249550c43a
XavierGomez <xavierg.gomez@students.salle.url.edu>
2021-12-30 00:34:19 +0100

    DAO. Revisar que sea correcto List<String>.
    Creo que ha de ser List<Trial[]>, pero en este caso no se como hacer el metodo readfiles.
    He comentado los metodos con retorno del Trial para que no marque error, pero mas adelante los usaremos.

--- src/persistence/LesProvesDAO.java
+++ src/persistence/LesProvesDAO.java
@@ -3,45 +3,30 @@ package persistence;
 import business.*;
 import com.google.gson.Gson;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LesProvesDAO {
-    private final static int ARTICLE = 1;
-    private final static int MASTER = 2;
-    private final static int PHD = 3;
-    private final static int BUDGET = 4;
     private static Gson gson = new Gson();
-    private List<Trial[]> trialsList = new ArrayList<>();
+    private List<String> trialsList = new ArrayList<>();
 
-    private File publicArticleCsvFile = new File("csv_files/publicArticle.csv");
-    private File studyMasterCsvFile = new File("csv_files/studyMaster.csv");
-    private File phdDefenseCsvFile = new File("csv_files/phdDefense.csv");
-    private File budgetRequestCsvFile = new File("csv_files/budgetRequest.csv");
-
-    private File publicArticleJsonFile = new File("json_files/publicArticle.json");
-    private File studyMasterJsonFile = new File("json_files/studyMaster.json");
-    private File phdDefenseJsonFile = new File("json_files/phdDefense.json");
-    private File budgetRequestJsonFile = new File("json_files/budgetRequest.json");
+    private Trial[] publicArticle = new PublicArticle[]{};
+    private Trial[] studyMaster = new StudyMaster[]{};
+    private Trial[] phDefense = new PhDefense[]{};
+    private Trial[] budgedRequest = new BudgedRequest[]{};
 
     public void writeJson () {
 
     }
 
-    public List<Trial[]> readJson () {
+    public List<String> readJson () {
         try {
-            FileReader fileReader1 = new FileReader(publicArticleJsonFile);
-            FileReader fileReader2 = new FileReader(studyMasterJsonFile);
-            FileReader fileReader3 = new FileReader(phdDefenseJsonFile);
-            FileReader fileReader4 = new FileReader(budgetRequestJsonFile);
-
-            trialsList.add(gson.fromJson(fileReader1, PublicArticle[].class));
-            trialsList.add(gson.fromJson(fileReader2, StudyMaster[].class));
-            trialsList.add(gson.fromJson(fileReader3, PhDefense[].class));
-            trialsList.add(gson.fromJson(fileReader4, BudgedRequest[].class));
+            trialsList.add(Arrays.toString(gson.fromJson(new FileReader("json_files/publicArticle.json"), PublicArticle[].class)));
+            trialsList.add(Arrays.toString(gson.fromJson(new FileReader("json_files/studyMaster.json"), StudyMaster[].class)));
+            trialsList.add(Arrays.toString(gson.fromJson(new FileReader("json_files/phdDefense.json"), PhDefense[].class)));
+            trialsList.add(Arrays.toString(gson.fromJson(new FileReader("json_files/budgetRequest.json"), BudgedRequest[].class)));
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
@@ -53,8 +38,37 @@ public class LesProvesDAO {
 
     }
 
-    public List<Trial[]> readCsv () {
+    public List<String> readCsv () {
+        try {
+            BufferedReader bufferedReaderPublicAticle = new BufferedReader(new FileReader("csv_files/publicArticle.csv"));
+            BufferedReader bufferedReaderStudyMaster = new BufferedReader(new FileReader("csv_files/studyMaster.csv"));
+            BufferedReader bufferedReaderPhDefense = new BufferedReader(new FileReader("csv_files/phdDefense.csv"));
+            BufferedReader bufferedReaderBudgetRequest = new BufferedReader(new FileReader("csv_files/budgetRequest.csv"));
+
+            trialsList.add(readfiles(bufferedReaderPublicAticle).toString());
+            trialsList.add(readfiles(bufferedReaderStudyMaster).toString());
+            trialsList.add(readfiles(bufferedReaderPhDefense).toString());
+            trialsList.add(readfiles(bufferedReaderBudgetRequest).toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return trialsList;
+    }
+
+    public ArrayList<String> readfiles (BufferedReader bufferedReader) {
+        ArrayList<String> trials = new ArrayList<>();
+
+        try {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                trials.add(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return trials;
     }
 }
