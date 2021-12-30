b39937d8713a6fad9c3667bb963d3486e7bc7e1c
Iris <irisquerol1998@gmail.com>
2021-12-30 16:16:54 +0100

    iqa-3-test

--- src/presentation/Menu.java
+++ src/presentation/Menu.java
@@ -7,44 +7,60 @@ import java.util.Scanner;
 public class Menu {
     private Scanner sc;
 
-    /** Global variable that stores a code that generates bold letters. */
+    /**
+     * Global variable that stores a code that generates bold letters.
+     */
     private static final String TEXT_BOLD = "\033[0;1m";
-    /** Global variable that stores a code that generates letters in red color. */
+    /**
+     * Global variable that stores a code that generates letters in red color.
+     */
     private static final String COLOR_RED = "\u001B[31m";
-    /** Global variable that stores a code that stops the generation of letters in red color. */
+    /**
+     * Global variable that stores a code that stops the generation of letters in red color.
+     */
     private static final String RESET_COLOR = "\u001B[0m";
 
-    /** Instantiates a new constructor */
-    public Menu () {
+    /**
+     * Instantiates a new constructor
+     */
+    public Menu() {
         this.sc = new Scanner(System.in);
     }
 
 
-    /** This method is called to show the format menu options on the screen. */
-    public void showFormatMenu () { // 4.1
+    /**
+     * This method is called to show the format menu options on the screen.
+     */
+    public void showFormatMenu() { // 4.1
         System.out.println(System.lineSeparator() +
                 "I) People’s Front of Engineering (CSV)" + System.lineSeparator() +
                 "II) Engineering People’s Front (JSON)" + System.lineSeparator());
     }
 
-    /** This method is called to show the role menu options on the screen. */
-    public void showRoleMenu () { // 4.2
+    /**
+     * This method is called to show the role menu options on the screen.
+     */
+    public void showRoleMenu() { // 4.2
         System.out.println(System.lineSeparator() +
                 "A) The Composer" + System.lineSeparator() +
                 "B) This year’s Conductor" + System.lineSeparator());
     }
 
-    /** This method is called to show the manager menu options on the screen. */
-    public void showManagerMenu () { // 4.3
+    /**
+     * This method is called to show the manager menu options on the screen.
+     */
+    public void showManagerMenu() { // 4.3
         System.out.println(System.lineSeparator() +
                 "1) Manage Trials" + System.lineSeparator() +
                 "2) Manage Editions" + System.lineSeparator() +
-                                        System.lineSeparator() +
+                System.lineSeparator() +
                 "3) Exit" + System.lineSeparator());
     }
 
-    /** This method is called to show the trial menu options on the screen. */
-    public void showTrialsMenu () { // 4.3.1
+    /**
+     * This method is called to show the trial menu options on the screen.
+     */
+    public void showTrialsMenu() { // 4.3.1
         System.out.println(System.lineSeparator() +
                 "a) Create Trial" + System.lineSeparator() +
                 "b) List Trials" + System.lineSeparator() +
@@ -53,8 +69,10 @@ public class Menu {
                 "e) Back" + System.lineSeparator());
     }
 
-    /** This method is called to show the editions menu options on the screen. */
-    public void showEditionsMenu () { // 4.3.2
+    /**
+     * This method is called to show the editions menu options on the screen.
+     */
+    public void showEditionsMenu() { // 4.3.2
         System.out.println(System.lineSeparator() +
                 "a) Create Edition" + System.lineSeparator() +
                 "b) List Editions" + System.lineSeparator() +
@@ -64,14 +82,18 @@ public class Menu {
                 "e) Back" + System.lineSeparator());
     }
 
-    /** This method is called to show the delete trial menu on the screen. */
-    public void showDeleteTrialMenu () {
+    /**
+     * This method is called to show the delete trial menu on the screen.
+     */
+    public void showDeleteTrialMenu() {
         System.out.println();
     }
 
-    /** This method is called to show the list of trials. */
-    public void showList (List<String> list) {
-        for(int i = 0; i < list.size(); i++) {
+    /**
+     * This method is called to show the list of trials.
+     */
+    public void showList(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
             // ALGO
         }
     }
@@ -83,7 +105,7 @@ public class Menu {
      * @param inputOption It is a String variable that we use to ask a user for information.
      * @return A value of primitive type integer entered by the user.
      */
-    public int askOptionInteger (String inputOption) {
+    public int askOptionInteger(String inputOption) {
         int number;
 
         while (true) {
@@ -106,7 +128,7 @@ public class Menu {
      * @param inputOption It is a String variable that we use to ask a user for information.
      * @return A value of non primitive type String entered by the user.
      */
-    public String askOptionString (String inputOption) {
+    public String askOptionString(String inputOption) {
         String answear;
 
         while (true) {
@@ -125,13 +147,13 @@ public class Menu {
      *
      * @param message String that contains a message to be shown.
      */
-    public void errorInput (String message) {
+    public void errorInput(String message) {
         System.out.println(TEXT_BOLD + COLOR_RED + System.lineSeparator() + message
                 + System.lineSeparator() + RESET_COLOR);
     }
 
 
-    public void showTittle () {
+    public void showTittle() {
         System.out.println(System.lineSeparator() +
                 " _____ _             _____      _       _" + System.lineSeparator() +
                 "/__   \\ |__   ___   /__   \\_ __(_) __ _| |___" + System.lineSeparator() +
@@ -146,12 +168,14 @@ public class Menu {
      *
      * @param message contains the message to be displayed.
      */
-    public void showMessage (String message) {
+    public void showMessage(String message) {
         System.out.println(message);
     }
 
 
-    /** It generates a new line space. */
+    /**
+     * It generates a new line space.
+     */
     public void createNewLine() {
         System.out.println();
     }
