00d9aa33f475c914205dd6938844971dbf490fc8
Iris <irisquerol1998@gmail.com>
2022-01-12 17:11:31 +0100

    iqa-12/01/commit final JSON ya funciona

--- src/persistence/JsonTrialDAO.java
+++ src/persistence/JsonTrialDAO.java
@@ -2,7 +2,6 @@ package persistence;
 
 import business.trial.*;
 import com.google.gson.*;
-import com.google.gson.stream.JsonWriter;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -11,6 +10,10 @@ import java.util.List;
 
 public class JsonTrialDAO implements TrialDAO {
     private static Gson gson = new Gson();
+    JsonArray arrayArticle = new JsonArray();
+    JsonArray arrayMaster = new JsonArray();
+    JsonArray arrayPHD = new JsonArray();
+    JsonArray arrayBudget = new JsonArray();
 
     private final String jsonArticlePath = "json_files/publicArticle.json";
     private final String jsonMasterPath = "json_files/studyMaster.json";
@@ -24,66 +27,78 @@ public class JsonTrialDAO implements TrialDAO {
 
     @Override
     public void writeAll(List<Trial> trials) {
-        Gson gsonBuild = new GsonBuilder().setPrettyPrinting().create();
         try {
-            new FileWriter(jsonArticlePath).close();//para borrar content
-            new FileWriter(jsonMasterPath).close();//para borrar content
-            new FileWriter(jsonPHDPath).close();//para borrar content
-            new FileWriter(jsonBudgetPath).close();//para borrar content
+            new FileWriter(jsonArticlePath).append("[]").close();//para borrar content
+            new FileWriter(jsonMasterPath).append("[]").close();//para borrar content
+            new FileWriter(jsonPHDPath).append("[]").close();//para borrar content
+            new FileWriter(jsonBudgetPath).append("[]").close();//para borrar content
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        JsonArray array = new JsonArray();
         for (Trial t : trials) {
-            JsonObject jsonObject = JsonParser.parseString(getObject(t)).getAsJsonObject();
-            array.add(jsonObject);
+            addToArray(t);
         }
+        writeArrays();
 
-        try {
-            OutputStream os = new FileOutputStream(jsonArticlePath, true);
-            os.write(gsonBuild.toJson(array).getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
-    private String getObject(Trial t) {
+    private void writeArrays() {
         Gson gsonBuild = new GsonBuilder().setPrettyPrinting().create();
-        String object = null;
+        try {
+            if (!arrayArticle.isEmpty()) {
+                OutputStream osArticle = new FileOutputStream(jsonArticlePath);
+                osArticle.write(gsonBuild.toJson(arrayArticle).getBytes());
+            }
+            if (!arrayMaster.isEmpty()) {
+                OutputStream osMaster = new FileOutputStream(jsonMasterPath);
+                osMaster.write(gsonBuild.toJson(arrayMaster).getBytes());
+            }
+            if (!arrayPHD.isEmpty()) {
+                OutputStream osPhd = new FileOutputStream(jsonPHDPath);
+                osPhd.write(gsonBuild.toJson(arrayPHD).getBytes());
+            }
+            if (!arrayBudget.isEmpty()) {
+                OutputStream osBudget = new FileOutputStream(jsonBudgetPath);
+                osBudget.write(gsonBuild.toJson(arrayBudget).getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addToArray(Trial t) {
         if (t instanceof PublicArticle) {
             checkDirectory(jsonArticlePath);
-            object = gson.toJson(t);
+            JsonObject jsonObject = JsonParser.parseString(gson.toJson(t)).getAsJsonObject();
+            arrayArticle.add(jsonObject);
         } else if (t instanceof StudyMaster) {
             checkDirectory(jsonMasterPath);
-            object = gson.toJson(t);
+            JsonObject jsonObject = JsonParser.parseString(gson.toJson(t)).getAsJsonObject();
+            arrayMaster.add(jsonObject);
         } else if (t instanceof PhDefense) {
             checkDirectory(jsonPHDPath);
-            object = gson.toJson(t);
+            JsonObject jsonObject = JsonParser.parseString(gson.toJson(t)).getAsJsonObject();
+            arrayPHD.add(jsonObject);
         } else if (t instanceof BudgedRequest) {
             checkDirectory(jsonBudgetPath);
-            object = gson.toJson(t);
+            JsonObject jsonObject = JsonParser.parseString(gson.toJson(t)).getAsJsonObject();
+            arrayBudget.add(jsonObject);
         }
-
-        return object;
     }
 
 
-    // Lo que esta comentado no va aqui, pero sirve para escribir los "[]" dentro de los JSONs.
-    // https://docs.oracle.com/javaee/7/api/javax/json/JsonArray.html
     // private void checkDirectoryJson (String path) throws CustomMessageException {
     private void checkDirectory(String path) {
-        //JsonObject jsonObject = new JsonObject();
-        //JsonArray jsonArray = new JsonArray();
         File directory = new File("json_files");
         directory.mkdir();
         File file = new File(path);
         if (!directory.exists()) directory.mkdirs();
         else if (!file.exists()) {
+
             try {
                 file.createNewFile();
-                // NO HACE FALTA ESCRIBIR [] pq antes se comprueba que trials es not empty por lo tanto se escribe si o si
                 try {
                     FileWriter writer = new FileWriter(file);
                     writer.append("[{}]");
@@ -95,8 +110,6 @@ public class JsonTrialDAO implements TrialDAO {
                     //throw new CustomMessageException("Error trying to write" + System.lineSeparator());
                 }
 
-
-                //Files.write(Paths.get(path), jsonObject.toString().getBytes());
             } catch (IOException ex) {
                 ex.printStackTrace();
                 //throw new CustomMessageException("Error trying to open" + System.lineSeparator());
